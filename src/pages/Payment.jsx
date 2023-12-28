cb16a926dab524136235cb0bb870f6d46a58ee51
wingled22 <pelayowindel@gmail.com>
2023-12-28 22:05:48 +0800

    added payment.jsx

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Col, Form, FormGroup, Input, Label, Row, Button, Table } from "reactstrap";
+import React, { useState } from 'react';
+import { Col, Form, FormGroup, Input, Row, Button, Table } from "reactstrap";
+import { Link } from "react-router-dom";
+
+
+
+import province from "../comp/address/province.json";
+import city from "../comp/address/city.json";
+import barangay from "../comp/address/barangay.json";
+
 
 const Payment = () => {
-    const handleSearch = (e) => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = async (e) => {
         e.preventDefault();
-        // Add your search logic here
-        console.log("Performing search...");
+
+        try {
+            const response = await fetch(`http://localhost:5034/api/Client/SearchClientByName?searchString=${encodeURIComponent(searchQuery)}`);
+            if (response.ok) {
+                const data = await response.json();
+                setSearchResults(data);
+            } else {
+                console.error('Error searching clients:', response.statusText);
+            }
+        } catch (error) {
+            console.error('An error occurred during the search:', error.message);
+        }
     };
 
     return (
@@ -18,7 +39,7 @@ const Payment = () => {
             }}>
                 <h4>Payment Register</h4>
                 <Row>
-                    <Col md="8"> {/* Adjust the column size based on your layout needs */}
+                    <Col md="8">
                         <Form onSubmit={handleSearch}>
                             <FormGroup>
                                 <Input
@@ -26,11 +47,13 @@ const Payment = () => {
                                     name="searchInput"
                                     id="searchInput"
                                     placeholder="Enter client's name ..."
+                                    value={searchQuery}
+                                    onChange={(e) => setSearchQuery(e.target.value)}
                                 />
                             </FormGroup>
                         </Form>
                     </Col>
-                    <Col md="4"> {/* Adjust the column size based on your layout needs */}
+                    <Col md="4">
                         <Button style={{ background: "#DFA248" }} onClick={handleSearch} className="w-100">Search</Button>
                     </Col>
                 </Row>
@@ -38,18 +61,38 @@ const Payment = () => {
                     <Col style={{ padding: 10 }}>
                         <Table bordered striped size='sm'>
                             <thead>
-                                <th>Name</th>
-                                <th>Address</th>
-                                <th>Actions</th>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Address</th>
+                                    <th>Actions</th>
+                                </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>sldkffje </td>
-                                    <td>sldkffje </td>
-                                    <td>
-                                        <Button size='sm'>sdfs</Button>
-                                    </td>
-                                </tr>
+                                {searchResults.map(client => (
+                                    <tr key={client.id}>
+                                        <td>{client.name}</td>
+                                        <td>{client.additionalAddressInfo == null
+                                            ? ""
+                                            : client.additionalAddressInfo + ", "}
+                                            {client.barangay == null
+                                                ? ""
+                                                : (barangay.find((item) => item.brgy_code === client.barangay) ||
+                                                    {}).brgy_name + ", "}
+                                            {client.city == null
+                                                ? ""
+                                                : (city.find((item) => item.city_code === client.city) ||
+                                                    {}).city_name + ", "}
+                                            {client.province == null
+                                                ? ""
+                                                : (province.find((item) => item.province_code === client.province) ||
+                                                    {}).province_name}</td>
+                                        <td>
+                                            <Link to={`/payment/clientloans/${client.id}`}>
+                                                <Button size='sm'>View Loan</Button>
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </Table>
                     </Col>
