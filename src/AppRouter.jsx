5120681828fa7ed8c2f55c9a4d80cd773c20a25e
wingled22 <pelayowindel@gmail.com>
2024-01-07 22:25:00 +0800

    ...

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -9,6 +9,7 @@ import Reports from "./pages/Reports.jsx";
 import App from './App.jsx';
 import ReportCBU from './reports/ReportCBU.jsx';
 import ReportPromisory from './reports/ReportPromisory.jsx';
+import Penalty from './pages/Penalty.jsx';
 
 const AppRouter = () => {
     return (
@@ -19,12 +20,11 @@ const AppRouter = () => {
                 <Route path="/payment" element={<Payment />} />
                 <Route path="/payment/clientloans/:clientId" element={<ClientLoans />} />
                 <Route path="/payment/paymentregister/:clientId/:scheduleId" element={<PaymentRegister />} />
-                <Route path="/reports" element={<Reports />} />
+                <Route path="/penalty" element={<Penalty />} />
                 <Route path="/reports" element={<Reports />} />
                 <Route path="/reports/cbu" element={<ReportCBU />} />
                 <Route path="/reports/promisory/:loanId" element={<ReportPromisory />} />
             </Route>
-
         </Routes>
     );
 }
